f4f1eb02fe8692844677c366084c9da2e8616787
saxtondrey <rm.kshr.hkkd0521@gmail.com>
2017-07-31 04:15:02 +0900

    get price information

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'dotenv/load'
 require 'amazon/ecs'
 
 class App
+  TARGET_ASINS = ['B01NCXFWIZ'].freeze
+
   def initialize
     Amazon::Ecs::debug = ENV['DEBUG']
     Amazon::Ecs.configure do |options|
@@ -11,7 +13,25 @@ class App
     end
   end
 
-  def request
-    Amazon::Ecs.item_search('Nintendo Switch', country: 'jp')
+  def response
+    @response ||= Amazon::Ecs.item_search('Nintendo Switch', country: 'jp', search_index: 'All', response_group: "ItemAttributes, OfferSummary")
+  end
+
+  def target_items
+    response.select { |item| TARGET_ASINS.include?(item.get('ASIN')) }
+  end
+
+  def price_xpath
+    'OfferSummary/LowestNewPrice/Amount'
+  end
+
+  def item_name_xpath
+    'ItemAttributes/Title'
+  end
+
+  def formatted
+    target_items.each do |item|
+      puts "#{item.get(item_name_xpath)} 新品価格 #{item.get(price_xpath)}円"
+    end
   end
 end
