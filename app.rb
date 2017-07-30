62df9378cd42f8ca060939c84a4a453a05c560ff
saxtondrey <rm.kshr.hkkd0521@gmail.com>
2017-07-31 04:59:16 +0900

    complete amazon part

--- app.rb
+++ app.rb
@@ -2,10 +2,16 @@ require 'dotenv/load'
 require 'amazon/ecs'
 
 class App
-  TARGET_ASINS = ['B01NCXFWIZ'].freeze
+  TARGET_NAME = 'Nintendo Switch'
+  TARGET_ASINS = ['B01NCXFWIZ', 'B0725V538Z'].freeze
+  BORDER_PRICE = 80000
+
+  PRICE_XPATH = 'OfferSummary/LowestNewPrice/Amount'.freeze
+  ITEM_NAME_XPATH = 'ItemAttributes/Title'.freeze
+  DETAIL_PAGE_URL_XPATH = 'DetailPageURL'.freeze
 
   def initialize
-    Amazon::Ecs::debug = ENV['DEBUG']
+    Amazon::Ecs::debug = ENV['mode'] == 'debug'
     Amazon::Ecs.configure do |options|
       options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
       options[:AWS_secret_key] = ENV['AWS_SECRET_KEY']
@@ -14,24 +20,34 @@ class App
   end
 
   def response
-    @response ||= Amazon::Ecs.item_search('Nintendo Switch', country: 'jp', search_index: 'All', response_group: "ItemAttributes, OfferSummary")
+    @response ||= Amazon::Ecs.item_search(TARGET_NAME, country: 'jp', search_index: 'All', response_group: "ItemAttributes, OfferSummary")
   end
 
   def target_items
-    response.select { |item| TARGET_ASINS.include?(item.get('ASIN')) }
+    response.items.select { |item| TARGET_ASINS.include?(item.get('ASIN')) }
   end
 
-  def price_xpath
-    'OfferSummary/LowestNewPrice/Amount'
+  def met_items
+    target_items.select { |item| item.get(PRICE_XPATH).to_i <= BORDER_PRICE }
+  end
+end
+
+class Formatter
+  def basic_info(item)
+    puts "#{item.get(App::ITEM_NAME_XPATH)} 新品価格 #{item.get(App::PRICE_XPATH)}円"
   end
 
-  def item_name_xpath
-    'ItemAttributes/Title'
+  def met_info(item)
+    puts "#{item.get(App::ITEM_NAME_XPATH)}が#{item.get(App::PRICE_XPATH)}円で販売開始されました。=> #{item.get(App::DETAIL_PAGE_URL_XPATH)}"
   end
+end
 
-  def formatted
-    target_items.each do |item|
-      puts "#{item.get(item_name_xpath)} 新品価格 #{item.get(price_xpath)}円"
-    end
-  end
+
+app = App.new
+formatter = Formatter.new
+begin
+  app.target_items.each { |item| formatter.basic_info(item) }
+  app.met_items.each { |item| formatter.met_info(item) }
+rescue Amazon::RequestError
+  puts 'request limit exceeded.'
 end
